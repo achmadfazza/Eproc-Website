81d4851a89587b4eb65f7c93afed3c6a92884ff3
faza <arkadyfaza@gmail.com>
2022-09-15 13:05:35 +0700

    fix

--- pages/register.js
+++ pages/register.js
@@ -5,7 +5,7 @@ function Register() {
     <div>
       <section className="h-screen">
         <div className="px-6 h-full text-gray-800">
-          <div className="flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-full g-6">
+          <div className="flex xl:justify-center lg:justify-between justify-center itembss-center flex-wrap h-full g-6">
             <div className="grow-0 shrink-1 md:shrink-0 basis-auto xl:w-6/12 lg:w-6/12 md:w-9/12 mb-12 md:mb-0">
               <img
                 src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
@@ -15,7 +15,7 @@ function Register() {
             <div className="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0">
               <form>
                 <h2 className="text-2xl text-center">Register Page</h2>
-                <div className="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5">
+                <div className="flex items-centerz my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5">
                   <p className="text-center font-semibold mx-4 mb-0">Register</p>
                 </div>
                 <div className="mb-6">
